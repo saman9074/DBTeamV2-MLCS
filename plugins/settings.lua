017ab3e1a1a2bb62cf8bdf9a52ac340feff4b51f
saman9074 <saman9074@yahoo.com>
2017-04-08 20:21:17 +0430

    Update settings.lua

--- plugins/settings.lua
+++ plugins/settings.lua
@@ -580,6 +580,7 @@ local function run(msg, matches)
 		elseif matches[1]:lower() == "remrules" or matches[1] == lang_text(msg.to.id, 'remrulesCommand') and not matches[2] and permissions(msg.from.id, msg.to.id, "settings") and redis:get("moderation_group: " .. msg.to.id) then
 			redis:del("settings:rules:" .. msg.to.id, matches[2])
 			send_msg(msg.to.id, lang_text(msg.to.id, 'rulesDefault'), 'md')
+		--[[
 		 elseif matches[1] == "all" or matches[1] == lang_text(msg.to.id, 'allCommand') and matches[3] and permissions(msg.from.id, msg.to.id, "settings") and redis:get("moderation_group: " .. msg.to.id) then
 			if matches[2] == 'media' or matches[2] == lang_text(msg.to.id, 'mediaCommand') then
 					if matches[3] == 'off' or matches[3] == lang_text(msg.to.id, 'offCommand') then
@@ -676,7 +677,8 @@ local function run(msg, matches)
 								send_msg(msg.to.id, lang_text(msg.to.id, 'grouponMSG'), 'md')
 						end
 			end
-	    elseif matches[1] == "all" or matches[1] == lang_text(msg.to.id, 'allCommand') and not matches[3] and permissions(msg.from.id, msg.to.id, "settings") and redis:get("moderation_group: " .. msg.to.id) then
+		]]--
+	    elseif matches[1] == "all" or matches[1] == lang_text(msg.to.id, 'allCommand') and permissions(msg.from.id, msg.to.id, "settings") and redis:get("moderation_group: " .. msg.to.id) then
 			if matches[2] == 'off' or matches[2] == lang_text(msg.to.id, 'offCommand') then
 			-- tgservices --
 			redis:set("settings:tgservices:" .. msg.to.id, true)
