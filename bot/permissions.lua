1d70a111ad918f02d472458f78213d5120d24e53
saman9074 <saman9074@yahoo.com>
2017-04-08 18:46:47 +0430

    Update permissions.lua

--- bot/permissions.lua
+++ bot/permissions.lua
@@ -1,3 +1,5 @@
+local lsocket  = require("lsocket")
+
 local sudos = {
     "lang_install",
     "promote_admin",
@@ -55,13 +57,25 @@ local function user_num(user_id, chat_id)
 	end
 end
 
+
+function sleep(sec)
+    lsocket.select(sec)
+end
+
+
 local function send_warning(user_id, chat_id, user_need)
 	if user_need == 3 then
 		send_msg(chat_id, lang_text(chat_id, 'require_sudo'), 'md')
+		sleep(5)
+		delete_msg(msg.to.id, msg.id)
 	elseif user_need == 2 then
 		send_msg(chat_id, lang_text(chat_id, 'require_admin'), 'md')
+		sleep(5)
+		delete_msg(msg.to.id, msg.id)
 	elseif user_need == 1 then
 		send_msg(chat_id, lang_text(chat_id, 'require_mod'), 'md')
+		sleep(5)
+		delete_msg(msg.to.id, msg.id)
 	end
 end
 
