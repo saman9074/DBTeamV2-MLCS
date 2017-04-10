a9d633f953d427edb33ea67b5d27b7c3b9f91591
saman9074 <saman9074@yahoo.com>
2017-04-10 12:42:03 +0430

    Create fun.lua

---
+++ plugins/fun.lua
@@ -0,0 +1,45 @@
+--[[
+-- Translate text using Google Translate.
+-- http://translate.google.com/translate_a/single?client=t&ie=UTF-8&oe=UTF-8&hl=en&dt=t&tl=en&sl=auto&text=hello
+--]]
+do
+
+function translate(source_lang, target_lang, text)
+  local path = "http://translate.google.com/translate_a/single"
+  -- URL query parameters
+  local params = {
+    client = "t",
+    ie = "UTF-8",
+    oe = "UTF-8",
+    hl = "en",
+    dt = "t",
+    tl = target_lang or "en",
+    sl = source_lang or "auto",
+    text = URL.escape(text)
+  }
+
+  local query = format_http_params(params, true)
+  local url = path..query
+
+  local res, code = https.request(url)
+  -- Return nil if error
+  if code > 200 then return nil end
+  local trans = res:gmatch("%[%[%[\"(.*)\"")():gsub("\"(.*)", "")
+
+  return trans
+end
+
+local function run(msg, matches)
+    if matches[1] == "tr" and matches[2] then
+    	local text = matches[1]
+    return translate(nil, nil, text)
+    end
+end
+
+return {
+  patterns = {
+     '^[!/#](tr) (.*)$'
+  }, 
+  run = run 
+}
+end
