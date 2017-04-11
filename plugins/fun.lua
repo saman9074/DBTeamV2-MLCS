fc58d4d7a1005cb7866962d978fd780da82e75b2
saman9074 <saman9074@yahoo.com>
2017-04-11 19:27:39 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -39,8 +39,7 @@ local function run(msg, matches)
     	local text = matches[1]
     return translate(nil, nil, text)
 	elseif matches[1] =="ch" and not matches[2] then
-			send_msg(msg.to.id, "bot: "..json_string[botsay], "md")
-
+			send_msg(msg.to.id, "bot: "..json_string['botsay'], "md")
     end
 end
 
