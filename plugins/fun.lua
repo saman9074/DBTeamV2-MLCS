34183af183d290d0003b342c854f40ba696db63c
saman9074 <saman9074@yahoo.com>
2017-04-11 21:51:12 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -38,12 +38,12 @@ local function run(msg, matches)
     if matches[1] == "tr" and matches[2] then
     	local text = matches[1]
     	return translate(nil, nil, text)
-	elseif matches[1] == "ch" and msg.reply_id == "360630346" then
- 				local url = "http://api.program-o.com/v2/chatbot/?bot_id=10&say="..matches.."&convo_id=exampleusage_2231232&format=json"
+	elseif matches[1] == "ch" and msg.reply_id and msg.replied.id == "360630346" then
+ 				local url = "http://api.program-o.com/v2/chatbot/?bot_id=10&say="..matches[2].."&convo_id=exampleusage_2231232&format=json"
   				local b,c = http.request(url)
 				if c ~= 200 then return nil end
 				local tab = json.decode(b)
-				reply_msg(msg.to.id, matches .. msg .. tab['botsay'],msg.id, 'md')
+				reply_msg(msg.to.id, matches[2] .. msg .. tab['botsay'],msg.id, 'md')
     end
 end
 
