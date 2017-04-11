857be06eff22457015c56474eb1431447c0ac6c5
saman9074 <saman9074@yahoo.com>
2017-04-11 18:48:58 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -3,6 +3,8 @@
 -- http://translate.google.com/translate_a/single?client=t&ie=UTF-8&oe=UTF-8&hl=en&dt=t&tl=en&sl=auto&text=hello
 --]]
 do
+	
+	local xml = require("xmlSimple.lua").newParser()
 
 function translate(source_lang, target_lang, text)
   local path = "http://translate.google.com/translate_a/single"
@@ -28,17 +30,23 @@ function translate(source_lang, target_lang, text)
 
   return trans
 end
+xml:loadFile("a.xml", base)
 
 local function run(msg, matches)
     if matches[1] == "tr" and matches[2] then
     	local text = matches[1]
     return translate(nil, nil, text)
+	elseif matches[1] =="ch" and not matches[2] then
+			test = xml.program_o.bot_name[]:value()
+			send_msg(msg.to.id, test, "md")
+
     end
 end
 
 return {
   patterns = {
-     '^[!/#](tr) (.*)$'
+     '^[!/#](tr) (.*)$',
+	 '^[!/#](ch)$'
   }, 
   run = run 
 }
