a58e4720afccea0fd5852d0720539b910ed266ec
saman9074 <saman9074@yahoo.com>
2017-04-11 19:33:14 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -39,7 +39,7 @@ local function run(msg, matches)
     	local text = matches[1]
     return translate(nil, nil, text)
 	elseif matches[1] =="ch" and not matches[2] then
-			send_msg(msg.to.id, "bot: "..tab['botsay'], "md")
+			reply_msg(msg.to.id, tab['botsay'],msg.id, 'md')
     end
 end
 
