5aa272f8fe860f5c561c3ad70b8af41e67738302
saman9074 <saman9074@yahoo.com>
2017-04-12 12:38:02 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -75,7 +75,7 @@ local function run(msg, matches)
   				local b,c = http.request(url)
 				if c ~= 200 then return nil end
 				local tab = json.decode(b)
-				reply_msg(msg.to.id, tab['botsay'],msg.id, 'md')
+				reply_msg(msg.to.id, tab['botsay'],msg.id, 'html')
 				
     end
 end
