ed48fcfad087e5e315d17f86480010ae9a68f38a
saman9074 <saman9074@yahoo.com>
2017-04-16 02:02:51 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -79,15 +79,9 @@ local function run(msg, matches)
 	elseif matches[1] == "wiki" and matches[2] then
 				local url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" .. matches[2]
   				local b,c = http.request(url)
-				--if c ~= 200 then return nil end--
-				--local tab = json.decode(b)--
-				--local encoded = json.encode( b, { indent = true } )--
-				--local decoded, pos, msg = json.decode( encoded )--
-				--if not decoded then--
-    					--reply_msg(msg.to.id, "*error!*" ,msg.id, 'md')--
-				--else--
-					   reply_msg(msg.to.id, "json: " .. b,msg.id, 'md')
-				--end--
+				if c ~= 200 then return nil end
+				local tab = json.decode(b)
+				reply_msg(msg.to.id, "json: " .. b,msg.id, 'md')
 				
 				
     end
