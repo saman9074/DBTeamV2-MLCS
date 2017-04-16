eb4e4f7a6c044c7f8a5c513bb25de1cad0206c07
saman9074 <saman9074@yahoo.com>
2017-04-16 16:50:06 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -112,7 +112,7 @@ local function run(msg, matches)
 					f:close()
 					send_document(msg.to.id, './data/userid_' .. msg.id .. "_" .. matches[2] ..  '.html')
 					--os.remove('./data/userid_' .. msg.id .. "_" .. matches[2] ..  '.txt')--
-				end
+				--end--
 	elseif matches[1] == "wiki" and matches[2] and not matches[3] then
 				local url = "http://api.golden3.ir/decoder/wiki.php?titles=" .. matches[2] .. "&lang=en"
 				local t,c = https.request(url)
@@ -136,7 +136,7 @@ local function run(msg, matches)
 					f:close()
 					send_document(msg.to.id, './data/userid_' .. msg.id .. "_" .. matches[2] ..  '.html')
 					--os.remove('./data/userid_' .. msg.id .. "_" .. matches[2] ..  '.txt')--
-				end
+				--end--
 				
 				
     end
