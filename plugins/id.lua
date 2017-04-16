4cc994967f2679231d5ebb7e2bdd1dfb4d8d0fae
saman9074 <saman9074@yahoo.com>
2017-04-16 23:51:01 +0430

    fix

--- plugins/id.lua
+++ plugins/id.lua
@@ -17,16 +17,17 @@ function getID_by_reply_cb(arg, msg)
 end
 
 local function run(msg, matches)
-	if not msg.reply_id then
-    	if not matches[2] then
-      		send_msg(msg.to.id, lang_text(msg.to.id, 'userID') .. " " .. msg.from.id ..  "\n" .. lang_text(msg.to.id, 'chatID') .. " " .. msg.to.id, "md")
-	else
-		if is_number(matches[2]) then
-        	resolve_id(matches[2], getIdUsername, msg.to.id)
-      	else
-        	resolve_username(matches[2], getUsernameId, msg.to.id)
-      	end
-	else
+  if not msg.reply_id then
+    if not matches[2] then
+      send_msg(msg.to.id, lang_text(msg.to.id, 'userID') .. " " .. msg.from.id ..  "\n" .. lang_text(msg.to.id, 'chatID') .. " " .. msg.to.id, "md")
+    else
+      if is_number(matches[2]) then
+        resolve_id(matches[2], getIdUsername, msg.to.id)
+      else
+        resolve_username(matches[2], getUsernameId, msg.to.id)
+      end
+    end
+  else
     send_ID_by_reply(msg.to.id, msg.reply_id)
   end
 end
@@ -45,8 +46,6 @@ function getUsernameId(chat, data)
   else
     send_msg(chat, "*ID:* `" .. data.id_ .. "`", "md")
   end
-
-		
 end
 
 return {
