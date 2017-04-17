1b70517095e189b7d085677d26d1dde9399124f3
saman9074 <saman9074@yahoo.com>
2017-04-17 22:46:44 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -94,6 +94,7 @@ local function run(msg, matches)
 				local tab = json.decode(b)
 				reply_msg(msg.to.id, tab['botsay'],msg.id, 'md')
 	elseif matches[1] == "ali" and matches[2] then
+				tdcli.changeChatTitle(msg.to.id, 'Typing',100, dl_cb, nil)
 				local url = "http://api.golden3.ir/chatbot/chatbot/conversation_start.php?bot_id=1&say=" .. matches[2] .. "&convo_id=userid_" .. msg.id
   				local b,c = http.request(url)
 				if c ~= 200 then return nil end
