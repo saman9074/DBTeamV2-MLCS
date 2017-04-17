2e8e8e88f07bbc51c35d2a2b742dba30d40deddb
saman9074 <saman9074@yahoo.com>
2017-04-17 21:30:58 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -139,7 +139,7 @@ local function run(msg, matches)
 				end	
 		elseif msg.reply_id and send_ID_by_reply(msg.to.id, msg.reply_id) then
 			--send_ID_by_reply(, )	--
-			
+			reply_msg(msg.to.id, "id: " .. userid_user ,msg.id, 'md')
 			if userid_user == "360630346" then
 				if matches[1] == "جوک" then
 					local url = "http://api.golden3.ir/chatbot/chatbot/conversation_start.php?bot_id=2&say=" .. matches[1] .. "&convo_id=userid_" .. msg.id
