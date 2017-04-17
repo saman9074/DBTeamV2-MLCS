0c5f6a3b76ab461249513f2d1bafe0e098457871
saman9074 <saman9074@yahoo.com>
2017-04-17 21:24:41 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -17,14 +17,6 @@ function split(s, delimiter)
     return result;
 end
 
-function send_ID_by_reply(channel_id, message_id)
-    get_msg_info(channel_id, message_id, getID_by_reply_cb_chat, false)
-end
-
-function getID_by_reply_cb_chat(arg, msg)
-		msg.sender_user_id_ = msg.sender_user_id_
-end
-
 
 	
 function sleep(n)
@@ -137,8 +129,9 @@ local function run(msg, matches)
 					run_bash("rm ./data/userid_" .. msg.id .. "_" .. matches[3] .. ".html")
 				end	
 		elseif msg.reply_id then
-			--send_ID_by_reply(msg.to.id, msg.reply_id)	--
-			if msg.from.id == "360630346" then
+			--send_ID_by_reply(, )	--
+			get_msg_info(msg.to.id, msg.reply_id, dl_cb, false)
+			if msg.sender_user_id_ == "360630346" then
 				if matches[1] == "جوک" then
 					local url = "http://api.golden3.ir/chatbot/chatbot/conversation_start.php?bot_id=2&say=" .. matches[1] .. "&convo_id=userid_" .. msg.id
   					local b,c = http.request(url)
